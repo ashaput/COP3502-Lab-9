0540e00f3ab96e2e2f29e8a32f9bc094badb7b66
maddybd22 <166658864+maddybd22@users.noreply.github.com>
2024-04-12 17:09:39 -0400

    Decoder
    I added the decoder function and implemented it into the menu.

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ def encode_password(password):
         encoded_password += str(x)
     return encoded_password
 
+def decode(code):
+    decoding = ''
+    for i in code:
+        decoding += str((int(i) - 3)%10)
+    return decoding
+
+
 if __name__ == '__main__':
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
-        option = input(int("Please enter an option: "))
-        password = input(str("Please enter your password to encode: "))
+        option = int(input("Please enter an option: "))
+
         if option == 1: #encode password
+            password = str(input("Please enter your password to encode: "))
             encoded = encode_password(password)
             print("Your password has been encoded and stored!")
         elif option == 2: #decode password
-            pass
+            print('The encoded password is:', encode_password(password), 'and the original password is', decode(encode_password(password)), '.')
         elif option == 3:
             break
 
